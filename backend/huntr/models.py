a20961cc906356395cf14521c1d652d7147f61c3
Lakota Rose <CheyLynn3@live.com>
2022-06-01 12:12:27 -0500

    Updated Models in Huntr App. Working on Serializers + URLS.

--- backend/huntr/models.py
+++ backend/huntr/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255) 
+    state = models.CharField(max_length=25)
+    zipcode = models.CharField(max_length=25)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    text = models.CharField(max_length=255)
+    listing_id = models.IntegerField()
+
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Review, null= True, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+
