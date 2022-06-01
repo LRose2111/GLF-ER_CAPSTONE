dc308fb89f58c61f2c86759b9e1fc658588cb931
Lakota Rose <CheyLynn3@live.com>
2022-06-01 13:05:12 -0500

    Made Migrations + Updated Admin, Models, URLS files.

--- backend/huntr/admin.py
+++ backend/huntr/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Listing, Review, Reply
 
-# Register your models here.
+admin.site.register(Listing) 
+admin.site.register(Review) 
+admin.site.register(Reply) 
--- backend/huntr/models.py
+++ backend/huntr/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from authentication.models import User
 
 class Listing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
